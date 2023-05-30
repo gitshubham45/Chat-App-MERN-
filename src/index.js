30af837ceaff8efc1a4e9842692063a77e258e3c
gitshubham45 <shubham16290@gmail.com>
2023-05-30 09:31:43 +0530

    frontend commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import ChatProvider from './Context/ChatProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+root.render
+(<ChatProvider>
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
+</ChatProvider>);
+
+
+
